c5f3d79907f4176550584c93db21594f53d9d134
paarthsoni <paarth2812@gmail.com>
2022-04-09 13:12:06 +0530

    db table

--- all_in_one_cp/admin.py
+++ all_in_one_cp/admin.py
@@ -1,1 +1,6 @@
+import imp
 from django.contrib import admin
+from all_in_one_cp.models import user_details, platform_details
+
+admin.site.register(user_details)
+admin.site.register(platform_details)
--- all_in_one_cp/models.py
+++ all_in_one_cp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from sqlalchemy import false, true
 
 # Create your models here.
+
+
+class user_details(models.Model):
+    name = models.CharField(max_length=200, null=False, unique=False)
+    username = models.CharField(
+        max_length=200, null=False, unique=True, primary_key=True)
+    password = models.CharField(max_length=200, null=False, unique=True)
+
+
+class platform_details(models.Model):
+    username = models.CharField(
+        max_length=200, null=False, unique=True, primary_key=True)
+    Leetcode_username = models.CharField(
+        max_length=200, null=False, unique=False)
+    Codeforces_username = models.CharField(
+        max_length=200, null=False, unique=False)
+    SPOJ_username = models.CharField(
+        max_length=200, null=False, unique=False)
+    Interviewbit_username = models.CharField(
+        max_length=200, null=False, unique=False)
+    Atcoder_username = models.CharField(
+        max_length=200, null=False, unique=False)
