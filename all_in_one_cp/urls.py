8550d809cf85ac9ebd22357ab26bd80c71d25632
paarthsoni <paarth2812@gmail.com>
2022-04-09 12:31:45 +0530

    second commit

--- all_in_one_cp/urls.py
+++ all_in_one_cp/urls.py
@@ -4,7 +4,12 @@ from django.urls import include, path
 from all_in_one_cp import views
 urlpatterns = [
 
-    path('', views.index, name="home"),
-
+    path('', views.home, name="home"),
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register"),
+    path('profile', views.profile, name="profile"),
+    path('explore_problems', views.explore_problems, name="problems"),
+    path('daily_coding', views.daily_coding, name="daily_coding"),
+    path('problem_of_the_day', views.problem_of_the_day, name="probken_of_the_day"),
 
 ]
--- all_in_one_cp/views.py
+++ all_in_one_cp/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render, HttpResponse
 
 
-def index(request):
-    return render(request, 'base.html')
+def home(request):
+    return render(request, 'home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def explore_problems(request):
+    return render(request, 'random_problems.html')
+
+
+def daily_coding(request):
+    return render(request, 'daily_coding.html')
+
+
+def problem_of_the_day(request):
+    return render(request, 'problem_of_the_day.html')
